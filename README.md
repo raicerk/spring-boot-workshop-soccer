004e9b8eb9a533ded96843c60605acb86038fe46
Carellano <cristian@23people.io>
2018-12-10 11:06:09 -0300

    hello world

--- README.md
+++
@@ -1,2 +0,0 @@
-# spring-boot-workshop-soccer
-Taller spring boot equipos y jugadores
---
+++ src/main/java/workshop/spring/boot/playerandteamworkshop/PlayerAndTeamWorkshopApplication.java
@@ -0,0 +1,12 @@
+package workshop.spring.boot.playerandteamworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerAndTeamWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerAndTeamWorkshopApplication.class, args);
+	}
+}
